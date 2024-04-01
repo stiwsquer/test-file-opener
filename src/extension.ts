4c1ebcd8a8a87ac0f1bc5dd8b2a24259dda3b2f2
Jakub Labryszewski <jakub.labryszewski@theaccessgroup.com>
2024-04-01 16:55:49 +0200

    Open test file related to the currently opened file

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,84 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as path from "path";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "test-file-opener" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('test-file-opener.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from test-file-opener!');
-	});
-
-	context.subscriptions.push(disposable);
+function getTestFileName(fileName: string, extension: string) {
+  if ([".tsx", ".jsx", ".js", ".ts"].includes(extension))
+    return `${fileName}.**${extension}`;
+  if ([".cs", ".swift"].includes(extension))
+    return `${fileName}Tests${extension}`;
+  if ([".java", ".php"].includes(extension))
+    return `${fileName}Test${extension}`;
+  if ([".py"].includes(extension)) return `test_${fileName}${extension}`;
+  if ([".rb", ".go", ".cpp"].includes(extension))
+    return `${fileName}_**${extension}`;
 }
 
-// This method is called when your extension is deactivated
+async function getFileToOpen(foundFiles: vscode.Uri[]) {
+  if (foundFiles.length === 1) {
+    return foundFiles[0].path;
+  }
+
+  const formattedFiles = foundFiles.map((file) => {
+    return {
+      label: path.basename(file.fsPath),
+      detail: file.fsPath,
+    };
+  });
+
+  const file = await vscode.window.showQuickPick(formattedFiles, {
+    matchOnDetail: true,
+  });
+
+  if (!file) return;
+  return file?.detail;
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log(
+    'Congratulations, your extension "test-file-opener" is now active!'
+  );
+
+  let disposable = vscode.commands.registerCommand(
+    "test-file-opener.openTestFile",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) return;
+
+      const filePath = editor.document.uri.fsPath;
+      const extension = path.extname(filePath);
+      const fileName = path.basename(filePath, extension);
+
+      const fileToSearch = getTestFileName(fileName, extension);
+
+      if (!fileToSearch) {
+        vscode.window.showInformationMessage(
+          `File extension is unsupported: ${extension}`
+        );
+        return;
+      }
+
+      const foundFiles = await vscode.workspace.findFiles(
+        `**/${fileToSearch}`,
+        "**/node_modules/**",
+        10
+      );
+
+      if (foundFiles.length === 0) {
+        vscode.window.showInformationMessage(
+          `No test files found for ${fileName}${extension}`
+        );
+        return;
+      }
+
+      let fileToOpen = await getFileToOpen(foundFiles);
+      if (!fileToOpen) return;
+
+      let document = await vscode.workspace.openTextDocument(fileToOpen);
+      vscode.window.showTextDocument(document);
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
 export function deactivate() {}
