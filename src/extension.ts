6eb19d74b9b039538a50521e961a71861d10c079
Jakub Labryszewski <jakub.labryszewski@theaccessgroup.com>
2024-04-15 19:14:10 +0200

    Generate test file if not exist

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,9 @@
 import * as vscode from "vscode";
 import * as path from "path";
+import OpenAI from "openai";
+import { Uri } from "vscode";
 
-function getTestFileName(fileName: string, extension: string) {
+function getTestFileNameSearchPattern(fileName: string, extension: string) {
   if ([".tsx", ".jsx", ".js", ".ts"].includes(extension))
     return `${fileName}.**${extension}`;
   if ([".cs", ".swift"].includes(extension))
@@ -33,39 +35,159 @@ async function getFileToOpen(foundFiles: vscode.Uri[]) {
   return file?.detail;
 }
 
-export function activate(context: vscode.ExtensionContext) {
+function getApiKey() {
+  return vscode.workspace.getConfiguration().get<string>("openAI.apiKey");
+}
+
+function setApiKey(apiKey: string) {
+  vscode.workspace
+    .getConfiguration()
+    .update("openAI.apiKey", apiKey, vscode.ConfigurationTarget.Global);
+}
+
+async function promptForApiKey() {
+  return await vscode.window.showInputBox({
+    prompt: "Please enter your OpenAI API key to use test generation feature.",
+    placeHolder: "API key",
+  });
+}
+
+async function checkApiKey() {
+  const apiKey = getApiKey();
+  if (!apiKey) {
+    const newApiKey = await promptForApiKey();
+    if (newApiKey) setApiKey(newApiKey);
+    return newApiKey;
+  }
+  return apiKey;
+}
+
+async function tryToCreateTestFile(
+  testFileUri: vscode.Uri,
+  fileExtension: string,
+  implementationFileContent: string,
+  apiKey?: string
+) {
+  const openAiApiKey = apiKey ?? (await checkApiKey());
+  if (!openAiApiKey) return;
+
+  try {
+    const completion = await generateTestFileContent(
+      openAiApiKey,
+      fileExtension,
+      implementationFileContent
+    );
+    const content = completion.choices[0].message.content;
+    createTestFile(testFileUri, content!);
+  } catch (err) {
+    if ((err as { status: number }).status === 401) {
+      vscode.window.showErrorMessage(
+        "Invalid OpenAI API key. Please update your API key."
+      );
+      const newApiKey = await promptForApiKey();
+      if (newApiKey) {
+        setApiKey(newApiKey);
+        tryToCreateTestFile(
+          testFileUri,
+          fileExtension,
+          implementationFileContent,
+          newApiKey
+        );
+      }
+      return;
+    }
+    vscode.window.showErrorMessage(
+      "Failed to generate test file. Please try again."
+    );
+    return;
+  }
+}
+
+async function generateTestFileContent(
+  openAiApiKey: string,
+  fileExtension: string,
+  implementationFileContent: string
+) {
+  const openai = new OpenAI({
+    apiKey: openAiApiKey,
+  });
+  return await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: `You are a program that based on the provided implementation file, generates the test file in programming language which files has ${fileExtension} extension. 
+            You are returning only the test file content as plain text - no explanations and no code formatting.`,
+        // For .tsx files, when mocking imports instead of using jest.mock use anonymous function, for example: jest.mock('react-router', () => ({useParams: () => ({queryParam: "param"}) }));???
+      },
+      {
+        role: "user",
+        content: `Write a test file for this code: ${implementationFileContent}`,
+      },
+    ],
+  });
+}
+
+async function createTestFile(testFileLocation: vscode.Uri, content: string) {
+  await vscode.workspace.fs.writeFile(
+    testFileLocation,
+    new TextEncoder().encode(content!)
+  );
+  vscode.window.showTextDocument(testFileLocation, { preview: false });
+}
+
+export async function activate(context: vscode.ExtensionContext) {
   console.log(
     'Congratulations, your extension "test-file-opener" is now active!'
   );
+  await checkApiKey();
 
   let disposable = vscode.commands.registerCommand(
     "test-file-opener.openTestFile",
     async () => {
-      const editor = vscode.window.activeTextEditor;
-      if (!editor) return;
+      const activeTextEditor = vscode.window.activeTextEditor;
+      if (!activeTextEditor) return;
 
-      const filePath = editor.document.uri.fsPath;
-      const extension = path.extname(filePath);
-      const fileName = path.basename(filePath, extension);
+      const implFilePath = activeTextEditor.document.uri.fsPath;
+      const dirname = path.dirname(implFilePath);
+      const implFileExtension = path.extname(implFilePath);
+      const implFileNameWithoutExtension = path.basename(
+        implFilePath,
+        implFileExtension
+      );
 
-      const fileToSearch = getTestFileName(fileName, extension);
-
-      if (!fileToSearch) {
+      const testFileNameSearchPattern = getTestFileNameSearchPattern(
+        implFileNameWithoutExtension,
+        implFileExtension
+      );
+      if (!testFileNameSearchPattern) {
         vscode.window.showInformationMessage(
-          `File extension is unsupported: ${extension}`
+          `File extension is unsupported: ${implFileExtension}`
         );
         return;
       }
 
       const foundFiles = await vscode.workspace.findFiles(
-        `**/${fileToSearch}`,
+        `**/${testFileNameSearchPattern}`,
         "**/node_modules/**",
         10
       );
 
       if (foundFiles.length === 0) {
-        vscode.window.showInformationMessage(
-          `No test files found for ${fileName}${extension}`
+        const response = await vscode.window.showInformationMessage(
+          `No test files found. Do you want to create a test file for ${implFileNameWithoutExtension}${implFileExtension}?`,
+          "Yes",
+          "No"
+        );
+        if (response !== "Yes") return;
+
+        tryToCreateTestFile(
+          Uri.file(
+            dirname +
+              `/${implFileNameWithoutExtension}.test${implFileExtension}`
+          ),
+          implFileExtension,
+          activeTextEditor.document.getText()
         );
         return;
       }
